0f691ee33e4c193b1b2bbc3f348fb21d89a476df
hiitoos <adrianhitos@gmail.com>
2022-03-04 19:34:40 +0100

    escape character

--- usingStrings.py
+++ usingStrings.py
@@ -6,3 +6,5 @@ print(var1+ " " + var2)
 varNum = 3
 print("Esto es el numero", varNum)
 print("El", varNum, "es primo", var2, "como lo ves?")
+#Escape char when single quote in string
+print('This is Drii\'s laptop')
