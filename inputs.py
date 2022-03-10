2dd9e5b00ef1e38d447f100420305b636b054b02
hiitoos <adrianhitos@gmail.com>
2022-03-10 08:01:37 +0100

    input int

--- inputs.py
+++ inputs.py
@@ -4,3 +4,6 @@ var1 = input()
 print("Escribiste:\n",var1)
 var2 = input("Aqui tambien puede escribir:\n")
 print("Tambien escribiste:\n",var2)
+#Casteando con int forzamos que todo lo que escriba sea entero
+var3 = int(input("Escribe un numero:\n"))
+print("Este es el numero:\n", var3)
