06e22173e13b26f7fa075a81a31f232aa66cdb43
hiitoos <adrianhitos@gmail.com>
2022-03-10 07:21:40 +0100

    input strings

---
+++ inputs.py
@@ -0,0 +1,6 @@
+#Con input recoge lo que el usuario escriba y cuando se imprime lo hace como STRING
+print("Introduce algo: ")
+var1 = input()
+print("Escribiste:\n",var1)
+var2 = input("Aqui tambien puede escribir:\n")
+print("Tambien escribiste:\n",var2)
